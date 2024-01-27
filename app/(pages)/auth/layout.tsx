ce487b19c48330531c38aed93b7a00e2ae95197e
Kirill <horushevskiykirill@gmail.com>
2024-01-27 22:36:38 +0200

    Set new animation background

--- app/(pages)/auth/layout.tsx
+++ app/(pages)/auth/layout.tsx
@@ -1,18 +1,22 @@
-// https://loading.io/asset/718234 || Animation bg
-// > Font
-import { Ubuntu } from "next/font/google";
-const font = Ubuntu({
-  subsets: ["latin"],
-  weight: ["400"],
-});
+import Image from "next/image";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="relative w-full h-screen bg-black ">
-        <img
-          src="/bg.svg"
-          alt=""
+        <Image
+          src={"/gradientBg.svg"}
+          alt="gradientBg"
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover z-0 pointer-events-none"
+          width={1000}
+          height={1000}
+        />
+        <Image
+          src={"/animBg.svg"}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover z-0 pointer-events-none"
+          alt="animationBg"
+          width={1000}
+          height={1000}
         />
         {children}
       </div>
