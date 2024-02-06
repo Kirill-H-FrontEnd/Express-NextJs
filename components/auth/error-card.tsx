2219ca5b1cff1474643d338bc8af99efe8d0f31e
Kirill <horushevskiykirill@gmail.com>
2024-02-06 13:23:01 +0200

    test

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -8,7 +8,10 @@ export const ErrorCard: FC = ({}) => {
   return (
     <Card className="w-[400px] shadow-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center rounded-md">
       <CardHeader>
-        <Header label="Oops" title="Error" />
+        <Header
+          label="Email already in use with different provider!"
+          title="Oops!"
+        />
       </CardHeader>
       <CardFooter>
         <BackButton href="/auth/login" label="Back to login" />
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -24,16 +24,8 @@ import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 // > Actions
 import { login } from "@/actions/login";
-import { useSearchParams } from "next/navigation";
 
-type TLoginForm = {};
-
-export const LoginForm: FC<TLoginForm> = ({}) => {
-  const searchParams = useSearchParams();
-  const urlError =
-    searchParams.get("error") === "OAuthAccountNotLinked"
-      ? "Email already in use with different provider!"
-      : "";
+export const LoginForm: FC = ({}) => {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -102,7 +94,7 @@ export const LoginForm: FC<TLoginForm> = ({}) => {
               )}
             />
           </div>
-          <FormError message={error || urlError} />
+          <FormError message={error} />
           <FormSuccess message={success} />
           <Button
             disabled={isPending}
