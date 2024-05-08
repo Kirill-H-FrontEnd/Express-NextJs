bdd2f88cbce0057f9bfe2514fd8149cb665c568d
Kirill <horushevskiykirill@gmail.com>
2024-05-08 16:12:11 +0300

    Commit[11/3]: Work in Hero.tsx

--- app/(root)/components/hero/Hero.tsx
+++ app/(root)/components/hero/Hero.tsx
@@ -33,82 +33,76 @@ export const Hero: FC = ({}) => {
     <section
       className={`${s.home} relative w-full h-screen bg-[url('/home/hero/animBg.svg')] bg-no-repeat bg-center bg-cover overflow-hidden select-none text-purple-800`}
     >
-      {/* Background */}
-      {/* <Image
-        src={"/home/gradientBg.svg"}
-        alt="gradientBg"
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full object-cover z-0 pointer-events-none select-none"
-        width={1000}
-        height={1000}
-      /> */}
-      <div className="container">
-        <motion.section
-          initial="offscreen"
-          whileInView="onscreen"
-          viewport={{ once: true }}
-          className={`${s.wrapper} relative z-10 grid place-items-center h-screen `}
-        >
-          <article className={`${s.article} text-center select-text`}>
-            <motion.h1
-              variants={Variants}
-              custom={1}
-              className={`${GeistSans.className} font-bold sm:font-extrabold text-white leading-[1.2] mb-4 text-balance
+      <div className="w-full h-screen bg-[url('/home/gradientBg.svg')] bg-no-repeat bg-center bg-cover">
+        <div className="container ">
+          <motion.section
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true }}
+            className={`${s.wrapper} relative z-10 grid place-items-center h-screen `}
+          >
+            <article className={`${s.article} text-center select-text`}>
+              <motion.h1
+                variants={Variants}
+                custom={1}
+                className={`${GeistSans.className} font-bold sm:font-extrabold text-white leading-[1.2] mb-4 text-balance
             `}
-            >
-              The{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-t from-purple-200 via-purple-600 to-[#2D1445] ">
-                Progressive
-              </span>{" "}
-              JavaScript Framework
-            </motion.h1>
+              >
+                The{" "}
+                <span className="text-transparent bg-clip-text bg-gradient-to-t from-purple-200 via-purple-600 to-[#2D1445] ">
+                  Progressive
+                </span>{" "}
+                JavaScript Framework
+              </motion.h1>
 
-            <motion.p
-              custom={2}
-              variants={Variants}
-              className="text-slate-300 leading-relaxed sm:leading-normal max-w-[900px] m-auto mb-8 font-normal"
-            >
-              Used by some of the world's largest companies, Protocol enables
-              you to create{" "}
-              <span className="text-transparent bg-clip-text bg-gradient-to-t from-violet to-white ">
-                high-quality web applications
-              </span>{" "}
-              with the power of JavaScript.
-            </motion.p>
-            <motion.div
-              custom={3}
-              variants={Variants}
-              className="grid sm:grid-cols-2-auto sm:justify-center sm:m-auto gap-3 pb-3"
-            >
-              <Button
-                as={Link}
-                href="/dashboard"
-                className="shadow-lg px-10 text-white rounded-full capitalize bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-violet to-black font-medium"
-                size={"md"}
+              <motion.p
+                custom={2}
+                variants={Variants}
+                className="text-slate-300 leading-relaxed sm:leading-normal max-w-[900px] m-auto mb-8 font-normal"
               >
-                get started
-              </Button>
-              <Button
-                className="shadow-lg px-10 text-black rounded-full capitalize bg-white font-semibold"
-                size={"md"}
+                Used by some of the world's largest companies, Protocol enables
+                you to create{" "}
+                <span className="text-transparent bg-clip-text bg-gradient-to-t from-violet to-white ">
+                  high-quality web applications
+                </span>{" "}
+                with the power of JavaScript.
+              </motion.p>
+              <motion.div
+                custom={3}
+                variants={Variants}
+                className="grid sm:grid-cols-2-auto sm:justify-center sm:m-auto gap-3 pb-3"
               >
-                Learn more
-              </Button>
-            </motion.div>
-            <Snippet
-              disableTooltip
-              symbol={`# ~`}
-              classNames={{
-                base: "bg-transparent text-gray",
-                pre: "font-medium text-sm",
-              }}
-              size="sm"
-            >
-              npx create-protocol-app@lates
-            </Snippet>
-          </article>
-        </motion.section>
+                <Button
+                  as={Link}
+                  href="/dashboard"
+                  className="shadow-lg px-10 text-white rounded-full capitalize bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-violet to-black font-medium"
+                  size={"md"}
+                >
+                  get started
+                </Button>
+                <Button
+                  className="shadow-lg px-10 text-black rounded-full capitalize bg-white font-semibold"
+                  size={"md"}
+                >
+                  Learn more
+                </Button>
+              </motion.div>
+              <Snippet
+                disableTooltip
+                symbol={`# ~`}
+                classNames={{
+                  base: "bg-transparent text-gray",
+                  pre: "font-medium text-sm",
+                }}
+                size="sm"
+              >
+                npx create-protocol-app@lates
+              </Snippet>
+            </article>
+          </motion.section>
+        </div>
       </div>
-      <div className="pointer-events-none ">
+      {/* <div className="pointer-events-none ">
         <div className="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50">
           <svg
             aria-hidden="true"
@@ -144,7 +138,7 @@ export const Hero: FC = ({}) => {
             </svg>
           </svg>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
